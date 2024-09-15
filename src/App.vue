08126b7e78c3a73dc073f6470f2b98c9b189c3a8
3 Dash <128896582+ColdGuy69@users.noreply.github.com>
2024-09-15 19:10:38 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -27,7 +27,7 @@
     <h1 class="webhook-deleter">ᴡᴇʙʜᴏᴏᴋ ɴᴜᴋᴇʀ/ᴅᴇʟᴇᴛᴇʀ</h1>
   </div>
 
- <label for="webhook-url" class="webhook-url-label">youtube.com/@XDash_o</label>
+ <label for="webhook-deleter" class="webhook-url-label">youtube.com/@XDash_o</label>
 
   <label for="webhook-url" class="webhook-url-label">ᴇɴᴛᴇʀ ᴡᴇʙʜᴏᴏᴋ ᴜʀʟ:</label>
   <div class="webhook-input">
